d01e8444eba20e9eb1ee67e28acd2c1bfdab6c66
sneha-latha-2004 <sneha22ec052@drttit.edu.in>
2026-03-25 15:35:14 +0530

    deco

---
+++ decorator.py
@@ -0,0 +1,13 @@
+# simple decorator
+def my_decorator(func):
+    def wrapper():
+        print("Before function")
+        func()
+        print("After function")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
